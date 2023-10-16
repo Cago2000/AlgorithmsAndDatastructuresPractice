95317adc7064e4338c762f11a6c22c655c38f695
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2023-10-16 16:54:06 +0200

    - Added insertion sort - Put random array in own function - Changed prints slightly

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,59 @@ public class Main {
     public static void main(String[] args)
     {
         bubbleSort();
+        insertionSort();
+    }
+
+    public static void insertionSort()
+    {
+        int[] input = makeRandomArray();
+        //input = new int[]{1,4,3,5,2}; // test array
+        //input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
+        int comparisons = 0;
+        boolean sorted = false;
+        System.out.println("Insertion Sort:");
+        printArray(input);
+
+        for(int i = 1; i < input.length;i++) // check if input is sorted
+        {
+            comparisons++;
+            if(input[i-1]>input[i])
+            {
+                break;
+            }
+            else if(i == input.length-1){
+                sorted = true;
+                System.out.println("Comparisons = " + comparisons);
+            }
+        }
+        if(!sorted){
+            for(int i = 1; i < input.length; i++)
+            {
+                for(int j = i; j > 0; j--)
+                {
+                    comparisons++;
+                    if(input[j] < input[j-1])
+                    {
+                        int temp;
+                        temp = input[j-1];
+                        input[j-1] = input[j];
+                        input[j] = temp;
+                        System.out.println("Swapped " + input[j-1] + " with "+ input[j] + ", Comparisons = " + comparisons);
+                        printArray(input);
+                    }
+                }
+            }
+        }
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------");
     }
 
     public static void bubbleSort()
     {
-        Random random = new Random();
-        int arraySize = random.nextInt(40) + 1; // random size 1-40
-        int[] input = new int[arraySize];
-
-        for (int i = 0; i < arraySize; i++) {
-            input[i] = random.nextInt(101); // random number 1-100
-        }
-
-        input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
+        int[] input = makeRandomArray();
+        //input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
         int comparisons = 0;
         boolean sorted = false;
+        System.out.println("Bubble Sort:");
         printArray(input);
 
         for(int i = 1; i < input.length;i++) // check if input is sorted
@@ -49,10 +87,10 @@ public class Main {
                         System.out.println("Swapped " + input[j] + " with "+ input[i] + ", Comparisons = " + comparisons);
                         printArray(input);
                     }
-
                 }
             }
         }
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------");
     }
 
     public static void printArray(int[] input) {
@@ -60,6 +98,17 @@ public class Main {
             System.out.print(i +" ");
         }
         System.out.println();
+    }
+
+    public static int[] makeRandomArray()
+    {
+        Random random = new Random();
+        int arraySize = random.nextInt(40) + 1; // random size 1-40
+        int[] array = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = random.nextInt(101); // random number 1-100
+        }
+        return array;
     }
 }
 
