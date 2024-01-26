ca3d4c48138137cbd9cfda83d64100eec205ccdc
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-01-26 09:44:47 +0100

    Made Util class, tidied stuff up

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 import java.util.Random;
 
 public class Main {
-    static int comparisons = 0;
+
     public static void main(String[] args)
     {
         //bubbleSort();
         //insertionSort();
-        System.out.println(euclideanAlgorithm(56,28));
+        System.out.println(euclideanAlgorithm(56,44));
     }
 
     public static void insertionSort()
     {
-        int[] input = makeRandomArray();
+        int comparisons = 0;
+        int[] input = Util.makeRandomArray();
         //input = new int[]{1,4,3,5,2}; // test array
         //input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
         comparisons = 0;
         boolean sorted;
         System.out.println("Insertion Sort:");
-        printArray(input);
-        sorted = arrayIsSorted(input);
+        Util.printArray(input);
+        sorted = Util.arrayIsSorted(input);
         if(!sorted)
         {
             for(int i = 1; i < input.length; i++)
@@ -33,24 +34,24 @@ public class Main {
                         input[j-1] = input[j];
                         input[j] = temp;
                         System.out.println("Swapped " + input[j-1] + " with "+ input[j] + ", Comparisons = " + comparisons);
-                        printArray(input);
+                        Util.printArray(input);
                     }
                 }
             }
         }
-        comparisons = 0;
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------");
     }
 
     public static void bubbleSort()
     {
-        int[] input = makeRandomArray();
+        int comparisons = 0;
+        int[] input = Util.makeRandomArray();
         //input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
         comparisons = 0;
         boolean sorted;
         System.out.println("Bubble Sort:");
-        printArray(input);
-        sorted = arrayIsSorted(input);
+        Util.printArray(input);
+        sorted = Util.arrayIsSorted(input);
         if(!sorted)
         {
             for(int i = 0; i < input.length; i++)
@@ -65,53 +66,12 @@ public class Main {
                         input[i] = input[j];
                         input[j] = temp;
                         System.out.println("Swapped " + input[j] + " with "+ input[i] + ", Comparisons = " + comparisons);
-                        printArray(input);
+                        Util.printArray(input);
                     }
                 }
             }
         }
-        comparisons = 0;
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------");
-    }
-
-    public static void printArray(int[] input)
-    {
-        for (int i:input)
-        {
-            System.out.print(i +" ");
-        }
-        System.out.println();
-    }
-
-    public static int[] makeRandomArray()
-    {
-        Random random = new Random();
-        int arraySize = random.nextInt(40) + 1; // random size 1-40
-        int[] array = new int[arraySize];
-        for (int i = 0; i < arraySize; i++)
-        {
-            array[i] = random.nextInt(101); // random number 1-100
-        }
-        return array;
-    }
-
-    public static boolean arrayIsSorted(int[] input)
-    {
-        boolean result = false;
-        for(int i = 1; i < input.length;i++) // check if input is sorted
-        {
-            comparisons++;
-            if(input[i-1]>input[i])
-            {
-                break;
-            }
-            else if(i == input.length-1)
-            {
-                result = true;
-                System.out.println("Comparisons = " + comparisons);
-            }
-        }
-        return result;
     }
 
     public static int euclideanAlgorithm(int a, int b)
---
+++ src/Util.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class Util {
+
+    public static boolean arrayIsSorted(int[] input)
+    {
+        int comparisons = 0;
+        boolean result = false;
+        for(int i = 1; i < input.length;i++) // check if input is sorted
+        {
+            comparisons++;
+            if(input[i-1]>input[i])
+            {
+                break;
+            }
+            else if(i == input.length-1)
+            {
+                result = true;
+                System.out.println("Comparisons = " + comparisons);
+            }
+        }
+        return result;
+    }
+
+    public static int[] makeRandomArray()
+    {
+        Random random = new Random();
+        int arraySize = random.nextInt(40) + 1; // random size 1-40
+        int[] array = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            array[i] = random.nextInt(101); // random number 1-100
+        }
+        return array;
+    }
+
+    public static void printArray(int[] input)
+    {
+        for (int i:input)
+        {
+            System.out.print(i +" ");
+        }
+        System.out.println();
+    }
+}
