407f08494d58fab2b759c6fb8a15039e89d12f04
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-01-26 11:15:58 +0100

    Start of implementation quickSort

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -6,20 +8,58 @@ public class Main {
     {
         //bubbleSort();
         //insertionSort();
-        System.out.println(euclideanAlgorithm(56,44));
+        //System.out.println(euclideanAlgorithm(56,44));
+        quickSortInit();
     }
 
+    public static void quickSortInit()
+    {
+        ArrayList<Integer> input = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+
+            int randomValue = random.nextInt(100);
+            input.add(randomValue);
+        }
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+
+        quickSort(input,left,right);
+    }
+    public static void quickSort(ArrayList<Integer> input,ArrayList<Integer> left,ArrayList<Integer> right)
+    {
+            if(input.size()>1) {
+                int pivot = input.size()/2;
+                System.out.println("Pivot: "+input.get(pivot));
+                for (int i = 0; i < input.size(); i++) {
+                    if (i == pivot) {
+                        continue;
+                    }
+                    if (input.get(i) >= input.get(pivot)) {
+                        left.add(input.get(i));
+                    }
+                    if (input.get(i) < input.get(pivot)) {
+                        right.add(input.get(i));
+                    }
+                }
+            System.out.print("Left: ");
+            for (int i:right) {System.out.print(i+" ");}
+            System.out.print("\nRight: ");
+            for (int i:left) {System.out.print(i +" ");}
+            System.out.println("\n");
+            quickSort(left,new ArrayList<>(), new ArrayList<>());
+            quickSort(right,new ArrayList<>(), new ArrayList<>());
+        }
+    }
     public static void insertionSort()
     {
         int comparisons = 0;
         int[] input = Util.makeRandomArray();
         //input = new int[]{1,4,3,5,2}; // test array
         //input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
-        comparisons = 0;
-        boolean sorted;
+        boolean sorted = Util.arrayIsSorted(input);
         System.out.println("Insertion Sort:");
         Util.printArray(input);
-        sorted = Util.arrayIsSorted(input);
         if(!sorted)
         {
             for(int i = 1; i < input.length; i++)
@@ -47,7 +87,6 @@ public class Main {
         int comparisons = 0;
         int[] input = Util.makeRandomArray();
         //input = new int[]{1,2,3,4,5,6,7,8,9,10}; // sorted array
-        comparisons = 0;
         boolean sorted;
         System.out.println("Bubble Sort:");
         Util.printArray(input);
